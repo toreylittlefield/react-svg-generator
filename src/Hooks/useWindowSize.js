8ffc09d1bd62057e1b2c646b72f184138804e348
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-15 10:52:47 -0400

    add resize custom hook

---
+++ src/Hooks/useWindowSize.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = React.useState({
+    winW: undefined,
+    winH: undefined,
+  });
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        winW: window.innerWidth,
+        winH: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+    // clean up
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+};
+
+export default useWindowSize;
