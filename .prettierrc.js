b2e65bec1df48cfaed35bea4473fe5047275b2ef
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-15 10:51:46 -0400

    add prettier rules and ignore

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/**
+ * Configure Prettier
+ *
+ * https://prettier.io/docs/en/configuration.html#basic-configuration
+ */
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  endOfLine: 'auto',
+};
