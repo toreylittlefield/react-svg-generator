a6c583ee33e2e30fddbdc5b3795fb7ec4d133617
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-18 09:08:11 -0400

    move export utils from useSVGExport customhook

---
+++ src/Utils/Utils.js
@@ -0,0 +1,93 @@
+const toastMessage = (msg = '') => {
+  const toastToRemove = document.querySelector('.toast-message');
+  if (toastToRemove) toastToRemove.remove();
+  const toastSection = document.createElement('div');
+  toastSection.classList.add('toast-message', 'open');
+  document.body.appendChild(toastSection);
+  toastSection.textContent = msg;
+  setTimeout(() => toastSection.remove(), 3000);
+};
+
+const clipboardFallBack = (code = '') => {
+  try {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'temp-input';
+    input.value = code;
+    document.body.appendChild(input);
+    input.style.cssText = `opacity: 0; position: fixed;`;
+    input.focus();
+    input.select();
+    const successful = document.execCommand('copy');
+    const msg = successful
+      ? 'Copied To Clipboard'
+      : 'Could Not Copy To Clipboard';
+    input.remove();
+    toastMessage(msg);
+  } catch (err) {
+    toastMessage(`Was not possible to copy the text: ${err}`);
+  }
+};
+
+export const copyToClipBoardSVG = () => {
+  const codeSelector = document.querySelector('code');
+  const code = codeSelector.textContent;
+  const { clipboard } = window.navigator;
+  if (!clipboard) clipboardFallBack(code);
+  if (clipboard)
+    clipboard
+      .writeText(code)
+      .then(() => {
+        const msg = 'Copied To Clipboard';
+        toastMessage(msg);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      });
+};
+
+export const downloadSVG = () => {
+  const svg = document.querySelector('#svg');
+  if (!svg) {
+    return toastMessage('Cannot download. Not SVG Found');
+  }
+  const blob = new Blob([svg.outerHTML], {
+    type: 'image/svg+xml;charset=utf-8',
+  });
+  const aTag = document.createElement('a');
+  aTag.download = 'generated-wave.svg';
+  aTag.href = window.URL.createObjectURL(blob);
+  aTag.click();
+  window.URL.revokeObjectURL(blob);
+  aTag.remove();
+  toastMessage('Downloading');
+};
+
+// // // const { gsap } = GSAP;
+
+// // // const animate = () => {
+// // //   const pathsSelector = document.querySelectorAll('path');
+// // //   let increment = 0;
+// // //   let percent = 1;
+// // //   gsap.to('path', {
+// // //     startFrom: {
+// // //       y: -300,
+// // //       x: -5,
+// // //       delay: -1,
+// // //     },
+// // //     y: -200,
+// // //     x: 5,
+// // //     duration: 2,
+// // //     stagger: {
+// // //       each: 0.1,
+// // //       from: 'edges',
+// // //       grid: 'auto',
+// // //       ease: 'sine.inOut',
+// // //       repeat: -1,
+// // //       yoyo: true,
+// // //     },
+// // //   });
+// // // };
+
+// // // animate()
