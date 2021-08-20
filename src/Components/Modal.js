e014eb111557c16068e92a552052d007adc2dd98
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-20 08:23:01 -0400

    get msgs from copy or download buttons and pass to toast

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import useSVGExport from '../Hooks/useSVGExport';
 import useRenderCount from '../Hooks/useRenderCount';
+import Toast from './Toast';
 
 // eslint-disable-next-line react/prop-types
 const Modal = ({ isModalOpen, setIsModalOpen, guiData }) => {
   const handleCloseModal = () => setIsModalOpen((prev) => !prev);
+  const [msg, setMsg] = useState('');
   const handleClassName = isModalOpen ? 'modal open' : 'modal close';
   const openCount = useRenderCount(isModalOpen);
   const {
@@ -13,27 +15,40 @@ const Modal = ({ isModalOpen, setIsModalOpen, guiData }) => {
     copyToClipBoardSVG = () => {},
     code = '',
   } = useSVGExport(isModalOpen, guiData);
+  const handleCopyToClipboard = () => {
+    const res = copyToClipBoardSVG();
+    setMsg(res);
+  };
+
+  const handleDownload = () => {
+    const res = downloadSVG();
+    setMsg(res);
+  };
 
   return (
-    <div className={openCount > 0 ? handleClassName : 'modal'}>
-      <h3>Copy Or Download SVG</h3>
-      <div className="code-container">
-        <pre>
-          <code>{code}</code>
-        </pre>
-        <div className="button-container">
-          <button id="copy" type="button" onClick={copyToClipBoardSVG}>
-            Copy To Clipboard
-          </button>
-          <button id="download" type="button" onClick={downloadSVG}>
-            Download SVG
-          </button>
-          <button id="close" type="button" onClick={handleCloseModal}>
-            Close
-          </button>
+    // eslint-disable-next-line react/jsx-fragments
+    <React.Fragment>
+      <div className={openCount > 0 ? handleClassName : 'modal'}>
+        <h3>Copy Or Download SVG</h3>
+        <div className="code-container">
+          <pre>
+            <code>{code}</code>
+          </pre>
+          <div className="button-container">
+            <button id="copy" type="button" onClick={handleCopyToClipboard}>
+              Copy To Clipboard
+            </button>
+            <button id="download" type="button" onClick={handleDownload}>
+              Download SVG
+            </button>
+            <button id="close" type="button" onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Toast msg={msg} setMsg={setMsg} isModalOpen={isModalOpen} />
+    </React.Fragment>
   );
 };
 
